f37c5f621354bbb2170eb5123d8f988784b96adf
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-02-17 20:17:21 +0100

    initialize app

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+const Home: NextPage = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session?.accessToken}
+        <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+};
+
+export default Home;
