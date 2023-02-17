d337085de943e377505f423908756ee3aa1f241e
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-02-17 20:12:08 +0100

    add spotify provider

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,113 @@
+import NextAuth, { Session, User } from 'next-auth';
+import { AdapterUser } from 'next-auth/adapters';
+import { JWT } from 'next-auth/jwt';
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+const SPOTIFY_AUTHORIZATION_URL =
+  'https://accounts.spotify.com/authorize?' +
+  new URLSearchParams({
+    prompt: 'consent',
+    access_type: 'offline',
+    response_type: 'code',
+    scope: 'user-read-email,playlist-read-private',
+  });
+
+async function refreshAccessToken(token: JWT) {
+  try {
+    const url =
+      'https://accounts.spotify.com/api/token?' +
+      new URLSearchParams({
+        client_id: process.env.SPOTIFY_CLIENT_ID!,
+        client_secret: process.env.SPOTIFY_CLIENT_SECRET!,
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken as string,
+      });
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: 'POST',
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires:
+        Date.now() + refreshedTokens.expires_at * 1000,
+      refreshToken:
+        refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: SPOTIFY_AUTHORIZATION_URL,
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account && user) {
+        return {
+          accessToken: account.access_token,
+          accessTokenExpires: account.expires_at! * 1000,
+          refreshToken: account.refresh_token,
+          user,
+        };
+      }
+      if (Date.now() < account?.expires_at! * 1000) {
+        return token;
+      }
+
+      return refreshAccessToken(token);
+    },
+    async session({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT;
+    }) {
+      session.user = token.user;
+      session.accessToken = token.accessToken;
+      session.error = token.error;
+
+      return session;
+    },
+  },
+});
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken: string;
+    accessTokenExpires: number;
+    refreshToken: string;
+    user: User | AdapterUser;
+    error?: string;
+  }
+}
+
+declare module 'next-auth' {
+  interface Session {
+    accessToken: string;
+    error?: string;
+  }
+}
