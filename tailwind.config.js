7141b9db9e6cf176e4d740bb177ca5e8b73524e0
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-02-18 23:11:48 +0100

    add custom colors + font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-space-grotesk)'],
+      },
+      colors: {
+        background: '#18181b',
+        text: '#e7e9ea',
+        white: '#ffffff',
+        primary: '#fd4927',
+      },
+    },
   },
   plugins: [],
-}
+};
