92cc61ed5197197738368a39867a1c4588f0322d
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-02-18 23:13:12 +0100

    add next font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
 import type { AppType } from 'next/app';
+import { Space_Grotesk } from '@next/font/google';
 import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-space-grotesk',
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -9,7 +15,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${spaceGrotesk.variable} font-sans h-full`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
