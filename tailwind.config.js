8a98bc96d643fa1d21ca1ef07ce1096587d3849e
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-02-21 19:45:05 +0100

    add few animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
         white: '#ffffff',
         primary: '#fd4927',
       },
+      animation: {
+        fadeIn:
+          'fadeIn 0.8s cubic-bezier(0.39, 0.575, 0.565, 1) both',
+        slideIn:
+          'slideIn 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both',
+      },
+      keyframes: {
+        slideIn: {
+          '0%': { transform: 'translateX(100%)' },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+        },
+      },
     },
   },
   plugins: [],
