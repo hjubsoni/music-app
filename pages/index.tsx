6c5819f59cb0fc76bed835e17ae988434e0d4824
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-02-23 19:41:36 +0100

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
+import PremiumPlans from '../components/PremiumPlans';
 import PremiumSection from '../components/PremiumSection';
 
 const Home: NextPage = () => {
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       <Header />
       <HeroSection />
       <PremiumSection />
+      <PremiumPlans />
     </div>
   );
 };
