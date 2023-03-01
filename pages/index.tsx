7b511713547016ff64ea6698b3eb70edd3c29096
hjubsoni <hubertwojnarowicz01@gmail.com>
2023-03-01 17:05:32 +0100

    chosing best way to retrive data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from 'next';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import { NextRequest } from 'next/server';
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
 import PremiumPlans from '../components/PremiumPlans';
 import PremiumSection from '../components/PremiumSection';
+import { getToken } from 'next-auth/jwt';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ token }) => {
   const { data: session } = useSession();
-
+  console.log(token);
   return (
     <div>
       <Header />
@@ -17,5 +19,16 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export async function getServerSideProps({ req }: any) {
+  const secret = process.env.NEXTAUTH_SECRET;
+  const token = await getToken({ req, secret });
+
+  return {
+    props: {
+      token: token?.accessToken,
+    },
+  };
+}
 
 export default Home;
